cb9a6e2cf5112f7602596cad6b06819ce8144eef
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2013-06-07 16:11:03 +0300

    Update .imfeelinglucky.sh

--- .imfeelinglucky.sh
+++ .imfeelinglucky.sh
@@ -4,7 +4,7 @@ set -e
 
 VIRTUALENV_BIN=`which virtualenv-2.7 || which virtualenv`
 PYTHON=`which python2.7 || which python`
-"$VIRTUALENV_BIN" --no-site-packages -p "$PYTHON"
+"$VIRTUALENV_BIN" --no-site-packages -p "$PYTHON" .
 
 # Let's enter the virtualenv
 . bin/activate
