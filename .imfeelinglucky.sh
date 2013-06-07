d93708b77704dacea2c6ae10703e45d5cb4adab4
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-06-07 16:02:04 +0300

    Update .imfeelinglucky.sh

--- .imfeelinglucky.sh
+++ .imfeelinglucky.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-virtualenv-2.7 --no-site-packages -p `which python2.7` .
+# we want to exit on errors
+set -e
+
+VIRTUALENV_BIN=`which virtualenv-2.7 || which virtualenv`
+PYTHON=`which python2.7 || which python`
+"$VIRTUALENV_BIN" --no-site-packages -p "$PYTHON"
+
+# Let's enter the virtualenv
 . bin/activate
+
+# Now we have
+PYTHON=`which python2.7 || which python`
 ln -s profiles/development.cfg buildout.cfg
-./bin/python2.7 bootstrap.py
+
+$PYTHON bootstrap.py
 ./bin/buildout
