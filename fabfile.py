c5b25445cd71f24114941e3488019f2b47fb055d
Andrew Mleczko <andrew@mleczko.net>
2013-07-15 12:35:58 +0200

    move fabric from production

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+"""
+Commands available to deploy using rt.commands
+"""
+# this is necessary to make the fabric commands available
+from fabric.api import env
+from rt.commands import *
+from rt.commands.project import *
+from rt.commands.buildout_components import *
+
+# This is the main python version
+env.python_version = '2.7'
+
+# This is the buildout script to use
+env.buildout_cfg = 'buildout.cfg'
+
+# And some hosts
+env.staging_user = ''
+env.staging_host = ''
+env.staging_dir = ''
+env.production_user = ''
+env.production_host = ''
+env.production_dir = ''
