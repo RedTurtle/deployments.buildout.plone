1df94dbabc24f8ed207be97bfeb8a9f7c3b21c30
Mauro Amico <mauro.amico@gmail.com>
2016-07-14 16:23:32 +0200

    script skeleton

---
+++ scripts/example.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+import argparse
+import logging
+import transaction
+from zope.component.hooks import getSite
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('scripts.example')
+
+class Application(object):
+    """
+    Usage:
+    ./bin/instance -O<plone_site_id> run scripts/example.py [--commit]
+    """
+
+    @classmethod
+    def main(cls, args_):
+        site = getSite()
+
+        print "......."
+
+        if args_.commit:
+            print "commit transaction"
+            transaction.commit()
+        else:
+            print "abort transaction (dry run)"
+            transaction.abort()
+        return 0
+
+    @classmethod
+    def run(cls):
+        parser = argparse.ArgumentParser(description=cls.__doc__)
+        parser.add_argument(
+             '--commit',
+             action='store_true',
+        )
+        sys.exit(cls.main(parser.parse_args(sys.argv[3:])))
+
+
+if __name__ == "__main__":
+    Application.run()
+
