4da2b4335a422168c72b443a2397f498513378ae
Piero Nicolli <piero.nicolli@redturtle.it>
2019-07-03 14:55:20 +0200

    Updated .imfeelinglucky.sh

--- .imfeelinglucky.sh
+++ .imfeelinglucky.sh
@@ -2,8 +2,8 @@
 # we want to exit on errors
 set -e
 
-VIRTUALENV_BIN=`which virtualenv-2.7 || which virtualenv`
-PYTHON=`which python2.7 || which python`
+VIRTUALENV_BIN=$(command -v virtualenv-2.7 || command -v virtualenv)
+PYTHON=$(command -v python2.7 || command -v python)
 "$VIRTUALENV_BIN" --no-site-packages -p "$PYTHON" .
 
 # Let's enter the virtualenv
@@ -11,6 +11,7 @@ PYTHON=`which python2.7 || which python`
 ./bin/pip install -r requirements.txt
 
 # Now we have
-PYTHON=`which python2.7 || which python`
+PYTHON=$(command -v python2.7 || command -v python)
 ln -s profiles/development.cfg buildout.cfg
 ./bin/buildout
+
